a4938070203c44c4fa80a379e2043d18c96e319e
Kirill Ermoshenko <ermoshenkokirill@yandex.ru>
2025-07-29 19:15:15 +0300

    Сделана пер вая версия теста на cypress

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  // множество разных настроек
+  transform: {
+    // '^.+\\.[tj]sx?$' для обработки файлов js/ts с помощью `ts-jest`
+    // '^.+\\.m?[tj]sx?$' для обработки файлов js/ts/mjs/mts с помощью `ts-jest`
+    '^.+\\.tsx?$': [ 'ts-jest',
+      { 
+        // настройки для ts-jest
+      },
+    ],
+  },
+  // moduleNameMapper: {}
+};
+
+export default config;
--- src/components/ui/burger-ingredients/burger-ingredients.tsx
+++ src/components/ui/burger-ingredients/burger-ingredients.tsx
@@ -48,6 +48,7 @@ export const BurgerIngredientsUI: FC<BurgerIngredientsUIProps> = memo(
             titleRef={titleBunRef}
             ingredients={buns}
             ref={bunsRef}
+            data-cy='buns'
           />
           <IngredientsCategory
             title='Начинки'
